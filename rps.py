642a1958c2f8fa45a5b2cd587faa57b4e447ce83
Paul Harper <paul@chartio.com>
2014-08-04 21:34:21 -0700

    Multiple improvements:
    Shows player names instead of numbers
    Cleans up finished games
    Allows you to list other players on the server

--- rps.py
+++ rps.py
@@ -2,66 +2,73 @@
 # Chat program
 import select
 import socket
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
 HOST = '0.0.0.0'
 PORT = 1338
 
 
-class ROCK(object):
-    name = 'rock'
+class Move(object):
+    superior = None
+    inferior = None
+
+    def __repr__(self):
+        return self.__class__.__name__
 
     def __cmp__(self, other):
-        if isinstance(other, PAPER):
+        if isinstance(other, self.superior):
             return -1
-        elif isinstance(other, SCISSORS):
+        elif isinstance(other, self.inferior):
             return 1
-        else:
+        elif isinstance(other, self.__class__):
             return 0
-
-
-class PAPER(object):
-    name = 'paper'
-
-    def __cmp__(self, other):
-        if isinstance(other, SCISSORS):
-            return -1
-        elif isinstance(other, ROCK):
-            return 1
         else:
-            return 0
+            raise TypeError('Can\'t compare {0} with {1}'.format(self, other))
 
 
-class SCISSORS(object):
-    name = 'scissors'
+class ROCK(Move):
+    def __init__(self):
+        self.superior = PAPER
+        self.inferior = SCISSORS
 
-    def __cmp__(self, other):
-        if isinstance(other, ROCK):
-            return -1
-        elif isinstance(other, PAPER):
-            return 1
-        else:
-            return 0
+
+class PAPER(Move):
+    def __init__(self):
+        self.superior = SCISSORS
+        self.inferior = ROCK
+
+
+class SCISSORS(Move):
+    def __init__(self):
+        self.superior = ROCK
+        self.inferior = PAPER
 
 
 class Game(object):
     winning_score = 3
 
-    def __init__(self, name):
+    def __init__(self, name, lobby):
         self.name = name
+        self.lobby = lobby
         self.winner = None
-        self.players = tuple()
+        self.players = set()
         self.moves = {}
         self.reset_score()
 
     def reset_score(self):
-        self.score = {0: 0, 1: 0}
+        self.score = defaultdict(lambda: 0)
 
     def add_player(self, player):
         if self.full:
-            raise Exception('Game is already full')
-        self.players += (player,)
-        return len(self.players) - 1
+            player.prompt('Game is already full')
+            return None
+        self.players.add(player)
+        return True
+
+    def other_player(self, player):
+        diff = self.players - {player}
+        (result,) = diff
+        return result
 
     def try_run(self):
         if self.full:
@@ -82,66 +89,87 @@ class Game(object):
     def full(self):
         return len(self.players) >= 2
 
+    @property
+    def gameover(self):
+        return self.winner is not None
+
+    def end_game(self):
+        for p in self.players:
+            p.game = None
+            p.prompt()
+        del self.lobby.games[self.name]
+
     def sendall(self, msg):
         for p in self.players:
             p.send(msg)
 
+    def show_score(self):
+        return '\n'.join('{0}: {1}'.format(p, self.score[p])
+                         for p in self.players)
+
     def play(self, player, move):
-        if self.winner is not None:
-            self.players[player].send(
-                'Player {} already won'.format(self.winner))
+        if player not in self.players:
+            player.send('You aren\'t a player in this game')
+            return
+
+        if self.gameover:
+            player.send('Player {} already won'.format(self.winner))
 
         if not self.full:
-            self.players[player].send(
-                'Wait until the game is full before playing...')
+            player.send('Wait until the game is full before playing...')
 
-        if move in ('r', 'rock'):
+        move = move.upper()
+        if move in ('R', 'ROCK'):
             self.moves[player] = ROCK()
-        elif move in ('p', 'paper'):
+        elif move in ('P', 'PAPER'):
             self.moves[player] = PAPER()
-        elif move in ('s', 'scissors'):
+        elif move in ('S', 'SCISSORS'):
             self.moves[player] = SCISSORS()
         else:
-            self.players[player].prompt(''.join([
+            player.prompt(''.join([
                 'Invalid move: "{}"'.format(move),
-                ' Choose one of: (r)ock, (p)aper or (s)cissors'
+                ' Choose one of: (R)OCK, (P)APER or (S)CISSORS'
             ]))
             return
 
         if len(self.moves) == 2:
             self.sendall('\n')
-            self.players[0].send(
-                'Player 1 threw {}'.format(self.moves[1].name))
-            self.players[1].send(
-                'Player 0 threw {}'.format(self.moves[0].name))
+            _players = list(self.players)
+            for p1, p2 in zip(_players, reversed(_players)):
+                p1.send('{0} threw {1}'.format(p2, self.moves[p2]))
 
             winner = None
-            if self.moves[0] > self.moves[1]:
-                self.score[0] += 1
-                winner = 0
-            elif self.moves[1] > self.moves[0]:
-                self.score[1] += 1
-                winner = 1
+            other = self.other_player(player)
+            if self.moves[player] > self.moves[other]:
+                winner = player
+            elif self.moves[other] > self.moves[player]:
+                winner = other
             if winner is not None:
+                self.score[winner] += 1
                 if self.score[winner] >= self.winning_score:
                     self.winner = winner
                     self.sendall('\n'.join([
                         'Player {} wins the game!'.format(winner),
-                        'Final score: {}'.format(self.score),
+                        self.show_score(),
                     ]))
                 else:
                     self.sendall('\n'.join([
                         'Player {} wins the round!'.format(winner),
-                        'Score: {}'.format(self.score),
+                        self.show_score(),
                     ]))
             else:
                 self.sendall('Tie')
 
             self.moves = {}
-            if self.winner is None:
+            if self.gameover:
+                self.end_game()
+            else:
                 self.prompt_move()
         else:
-            self.players[player].send('Waiting for other player to play...')
+            player.send('Waiting for other player to play...')
+
+    def __repr__(self):
+        return self.name
 
 
 class Lobby(object):
@@ -150,13 +178,13 @@ class Lobby(object):
 
     def new_game(self, name):
         if name in self.games:
-            return Exception('Name "{}" taken'.format(name))
-        game = Game(name)
+            return 'Name "{}" taken'.format(name)
+        game = Game(name, lobby=self)
         self.games[name] = game
         return game
 
     def list_games(self):
-        ls = '\n'.join('- {} ({})'.format(n, 'full' if g.full else 'open')
+        ls = '\n'.join('    {} ({})'.format(n, 'full' if g.full else 'open')
                        for n, g in self.games.iteritems())
         if not ls:
             ls = 'No games'
@@ -172,6 +200,7 @@ class Lobby(object):
             '    c <name>: create new game with <name>',
             '    j <name>: join existing game with <name>',
             '    l: list games',
+            '    who: list players',
         ])
 
 
@@ -179,7 +208,7 @@ class Player(object):
     def __init__(self, sock, lobby):
         self.socket = sock
         self.lobby = lobby
-        self.player_id = None
+        self.name = None
         self.game = None
 
     def prompt(self, txt=''):
@@ -187,10 +216,13 @@ class Player(object):
             txt += '\n'
         game_prompt = ''
         if self.game:
-            game_prompt = 'player {0} in {1}'.format(
-                self.player_id, self.game.name)
-        txt += '{} > '.format(game_prompt)
+            game_prompt = 'playing "{0}" against "{1}" '.format(
+                self.game.name, self.game.other_player(self))
+        txt += '{}> '.format(game_prompt)
         self.socket.send(txt)
+
+    def prompt_name(self):
+        self.socket.send('Please enter your name: ')
 
     def send(self, txt):
         if txt and not txt.endswith('\n'):
@@ -198,10 +230,10 @@ class Player(object):
         self.socket.send(txt)
 
     def create_game(self, name):
-        try:
-            self.lobby.new_game(name)
-        except Exception as e:
-            self.socket.send(str(e) + '\n')
+        msg = self.lobby.new_game(name)
+        if isinstance(msg, basestring):
+            self.prompt(msg)
+            return
         self.join_game(name)
 
     def join_game(self, name):
@@ -211,16 +243,19 @@ class Player(object):
         elif game.full:
             self.prompt('Game is full')
         else:
-            self.player_id = game.add_player(self)
+            game.add_player(self)
             self.game = game
             if not self.game.try_run():
                 self.send('Waiting for other player...')
 
     def play(self, move):
-        self.game.play(self.player_id, move)
+        self.game.play(self, move)
 
     def fileno(self):
         return self.socket.fileno()
+
+    def __repr__(self):
+        return self.name
 
 
 def main():
@@ -245,9 +280,7 @@ def main():
                 player = Player(new_client, lobby)
                 read_list.append(player)
                 write_list.append(player)
-                player.send(
-                    'Welcome to Rock Paper Scissors! Type "?" for help')
-                player.prompt()
+                player.prompt_name()
             elif isinstance(sock, Player):
                 player = sock
                 data = player.socket.recv(1024)
@@ -260,10 +293,23 @@ def main():
                 if player.game:
                     player.play(data)
                 else:
+                    if not player.name:
+                        if data:
+                            player.name = data
+                            player.prompt('Welcome to Rock Paper Scissors! '
+                                          'Type "?" for help')
+                        else:
+                            player.prompt_name()
+                        continue
+
                     if data == '?':
                         player.prompt(lobby.help())
                     elif data == 'l':
                         player.prompt(lobby.list_games())
+                    elif data == 'who':
+                        players = ['    {}'.format(p) for p in read_list
+                                   if isinstance(p, Player)]
+                        player.prompt('\n'.join(p for p in players))
                     elif data.startswith('c '):
                         name = data[2:]
                         player.create_game(name)
